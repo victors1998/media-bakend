a50c537e2340f1501bfb3078bf2f604fb4f7ae03
victors1998 <veusebiosoto@gmail.com>
2022-06-28 16:37:47 -0500

    v1

---
+++ src/main/java/com/victor/MediaBakendApplication.java
@@ -0,0 +1,13 @@
+package com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaBakendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/victor/model/Paciente.java
@@ -0,0 +1,5 @@
+package com.victor.model;
+
+public class Paciente {
+
+}
